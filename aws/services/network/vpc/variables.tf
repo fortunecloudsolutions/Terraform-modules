d4ef4f1b1366b0e18d5ccee00513f30593eabe60
vamsikrishnabandari <hr@fcsglobal.org>
2024-05-22 23:45:49 -0500

    added files

--- aws/services/network/vpc/variables.tf
+++ aws/services/network/vpc/variables.tf
@@ -3,29 +3,6 @@ variable "region" {
   type        = string
 }
 
-variable "vault_addr" {
-  description = "The Vault URL to which vault sends and requests data"
-  type        = string
-}
-variable "vault_token" {
-  description = "The vault token, used for authorization"
-  type        = string
-}
-
-variable "max_lease_seconds" {
-  description = "Specifies the maximum time-to-live. If set, this overrides the global default. Must be a valid duration string"
-  type        = string
-}
-
-variable "backend" {
-  description = "Backend is the provider"
-  type        = string
-}
-variable "role" {
-  description = "Role is usually resources"
-  type        = string
-}
-
 variable "cidr_vpc" {
   description = "The CIDR block for the VPC."
   type        = string
