d7733f34725274f06de801c48ddbcc2400376e27
Vinod Kumar <gsvinodkumar27@gmail.com>
2024-06-17 12:15:00 +0530

    adding modified files

--- production/variables.tf
+++ production/variables.tf
@@ -1,272 +1,285 @@
-variable "env" {
-  description = "The environment name"
-  type        = string
-}
-
 variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
+  description = "CIDR block for the VPC"
   type        = string
 }
 
 variable "enable_dns_support" {
   description = "Enable DNS support in the VPC"
   type        = bool
+  default     = true
 }
 
 variable "enable_dns_hostnames" {
   description = "Enable DNS hostnames in the VPC"
   type        = bool
+  default     = true
 }
 
 variable "instance_tenancy" {
-  description = "The instance tenancy attribute for the VPC"
+  description = "The tenancy of instances launched into the VPC"
   type        = string
-}
-
-variable "availability_zones" {
-  description = "A list of availability zones in the region"
-  type        = list(string)
+  default     = "default"
 }
 
 variable "map_publicip" {
-  description = "Map public IP on launch"
+  description = "Should be true if you want to provision instances with public IP addresses"
   type        = bool
+  default     = false
 }
 
 variable "assign_ipv6_address_on_creation" {
   description = "Assign IPv6 address on subnet creation"
   type        = bool
+  default     = false
 }
 
-variable "subnets_az_a_cidr" {
-  description = "List of CIDR blocks for subnets in AZ A"
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "private_subnet_a" {
+  description = "CIDR blocks for subnets in availability zone A"
   type        = map(string)
 }
 
-variable "subnets_az_b_cidr" {
-  description = "List of CIDR blocks for subnets in AZ B"
+variable "private_subnet_b" {
+  description = "CIDR blocks for subnets in availability zone B"
   type        = map(string)
 }
 
 variable "route_destination_cidr_block" {
-  description = "CIDR block for the route"
-  type        = string
-}
-
-variable "internet_gateway_id" {
-  description = "The ID of the Internet Gateway"
+  description = "Destination CIDR block for the route"
   type        = string
 }
 
 variable "bucket_names" {
-  description = "The list of bucket names"
+  description = "List of S3 bucket names"
   type        = list(string)
 }
 
 variable "s3_acl" {
-  description = "The ACL for the S3 buckets"
+  description = "ACL policy for S3 buckets"
   type        = string
+  default     = "private"
 }
 
 variable "ami_id" {
-  description = "The AMI ID for the EC2 instances"
+  description = "AMI ID for EC2 instances"
   type        = string
 }
 
 variable "instance_type" {
-  description = "The instance type for the EC2 instances"
+  description = "Instance type for EC2 instances"
   type        = string
 }
 
 variable "security_group_ids" {
-  description = "The list of security group IDs"
+  description = "Map of security group IDs for each instance"
   type        = map(list(string))
 }
 
 variable "key_name" {
-  description = "The key name for the EC2 instances"
+  description = "Key name for EC2 instances"
   type        = string
 }
 
 variable "alb_names" {
-  description = "The list of ALB names"
+  description = "List of ALB names"
   type        = list(string)
 }
 
 variable "alb_internal" {
-  description = "Whether the ALB is internal"
+  description = "Boolean to indicate if ALB should be internal"
   type        = bool
 }
 
 variable "alb_security_group_ids" {
-  description = "The security group IDs for the ALBs"
+  description = "Map of ALB security group IDs"
   type        = map(list(string))
 }
 
 variable "alb_idle_timeout" {
-  description = "The idle timeout for the ALB"
+  description = "Idle timeout for ALB"
   type        = number
+  default     = 60
 }
 
 variable "enable_alb_delete_via_awsapi" {
-  description = "Enable ALB delete via AWS API"
+  description = "Enable ALB deletion via AWS API"
   type        = bool
 }
 
 variable "alb_ip_address_type" {
-  description = "The IP address type for the ALB"
+  description = "IP address type for ALB"
   type        = string
 }
 
 variable "access_log_bucket_name" {
-  description = "The bucket name for ALB access logs"
+  description = "S3 bucket name for ALB access logs"
   type        = string
 }
 
 variable "enable_access_logs" {
-  description = "Enable access logs for the ALB"
+  description = "Enable access logs for ALB"
   type        = bool
 }
 
 variable "target_group_names" {
-  description = "The list of target group names"
+  description = "List of target group names"
   type        = list(string)
 }
 
 variable "target_ports" {
-  description = "The target ports for the target groups"
+  description = "Map of target ports for each target group"
   type        = map(number)
 }
 
 variable "target_protocol" {
-  description = "The target protocol for the target groups"
+  description = "Protocol for target groups"
   type        = string
+  default     = "HTTP"
 }
 
 variable "deregistration_delay" {
-  description = "The deregistration delay for the target groups"
+  description = "Deregistration delay for target groups"
   type        = number
+  default     = 300
 }
 
 variable "slow_start" {
-  description = "The slow start duration for the target groups"
+  description = "Slow start duration for target groups"
   type        = number
+  default     = 0
 }
 
 variable "load_balancing_algorithm_type" {
-  description = "The load balancing algorithm type for the target groups"
+  description = "Load balancing algorithm type for target groups"
   type        = string
+  default     = "round_robin"
 }
 
 variable "target_type" {
-  description = "The target type for the target groups"
+  description = "Target type for target groups"
   type        = string
+  default     = "instance"
 }
 
 variable "sticky_type" {
-  description = "The sticky session type for the target groups"
+  description = "Sticky type for target groups"
   type        = string
+  default     = "lb_cookie"
 }
 
 variable "sticky_cookie_duration" {
-  description = "The sticky cookie duration for the target groups"
+  description = "Sticky cookie duration for target groups"
   type        = number
+  default     = 86400
 }
 
 variable "sticky_enabled" {
-  description = "Enable sticky sessions for the target groups"
+  description = "Enable sticky sessions for target groups"
   type        = bool
+  default     = false
 }
 
 variable "health_check_enabled" {
-  description = "Enable health checks for the target groups"
+  description = "Enable health checks for target groups"
   type        = bool
+  default     = true
 }
 
 variable "health_check_interval" {
-  description = "The health check interval for the target groups"
+  description = "Health check interval for target groups"
   type        = number
+  default     = 30
 }
 
 variable "health_check_path" {
-  description = "The health check path for the target groups"
+  description = "Health check path for target groups"
   type        = string
 }
 
 variable "health_check_port" {
-  description = "The health check port for the target groups"
-  type        = string
+  description = "Health check port for target groups"
+  type        = number
+  default     = 80
 }
 
 variable "health_check_protocol" {
-  description = "The health check protocol for the target groups"
+  description = "Health check protocol for target groups"
   type        = string
+  default     = "HTTP"
 }
 
 variable "health_check_timeout" {
-  description = "The health check timeout for the target groups"
+  description = "Health check timeout for target groups"
   type        = number
+  default     = 5
 }
 
 variable "healthy_threshold" {
-  description = "The healthy threshold for the target groups"
+  description = "Healthy threshold for target groups"
   type        = number
+  default     = 3
 }
 
 variable "unhealthy_threshold" {
-  description = "The unhealthy threshold for the target groups"
+  description = "Unhealthy threshold for target groups"
   type        = number
+  default     = 3
 }
 
 variable "health_check_success_resp_code" {
-  description = "The success response code for the health checks"
+  description = "Health check success response code for target groups"
   type        = string
+  default     = "200"
 }
 
 variable "sg_names" {
-  description = "The list of security group names"
-  type        = list(string)
+  description = "Map of security group names"
+  type        = map(string)
 }
 
 variable "sg_descriptions" {
-  description = "The descriptions for the security groups"
+  description = "Map of security group descriptions"
   type        = map(string)
 }
 
 variable "security_group_descriptions" {
-  description = "The detailed descriptions for the security groups"
+  description = "Map of security group descriptions"
   type        = map(object({
-    ingress_desc             = string
-    ingress_fport            = number
-    ingress_tport            = number
-    ingress_protocol         = string
-    ingress_cidr             = string
-    ingress_ipv6_cidr        = string
-    ingress_prefix_ids       = list(string)
-    ingress_security_groups  = list(string)
-    egress_desc              = string
-    egress_fport             = number
-    egress_tport             = number
-    egress_protocol          = string
-    egress_cidr              = string
-    egress_ipv6_cidr         = string
-    egress_prefix_ids        = list(string)
-    egress_security_groups   = list(string)
+    ingress_desc            = string
+    ingress_fport           = number
+    ingress_tport           = number
+    ingress_protocol        = string
+    ingress_cidr            = string
+    ingress_ipv6_cidr       = string
+    ingress_prefix_ids      = list(string)
+    ingress_security_groups = list(string)
+    egress_desc             = string
+    egress_fport            = number
+    egress_tport            = number
+    egress_protocol         = string
+    egress_cidr             = string
+    egress_ipv6_cidr        = string
+    egress_prefix_ids       = list(string)
+    egress_security_groups  = list(string)
   }))
 }
 
 variable "db_subnet_group_name" {
-  description = "The name of the DB subnet group"
+  description = "Name of the DB subnet group"
   type        = string
 }
 
 variable "db_subnet_group_description" {
-  description = "The description of the DB subnet group"
+  description = "Description of the DB subnet group"
   type        = string
 }
 
 variable "db_identifier" {
-  description = "The identifier for the DB instance"
+  description = "Identifier for the DB instance"
   type        = string
 }
 
@@ -276,27 +289,27 @@ variable "db_allow_major_version_upgrade" {
 }
 
 variable "db_auto_minor_version_upgrade" {
-  description = "Auto minor version upgrade for the DB instance"
+  description = "Allow minor version upgrade for the DB instance"
   type        = bool
 }
 
 variable "db_engine" {
-  description = "The engine for the DB instance"
+  description = "Database engine for the DB instance"
   type        = string
 }
 
 variable "db_engine_version" {
-  description = "The engine version for the DB instance"
+  description = "Database engine version for the DB instance"
   type        = string
 }
 
 variable "db_instance_class" {
-  description = "The instance class for the DB instance"
+  description = "Instance class for the DB instance"
   type        = string
 }
 
 variable "db_allocated_storage" {
-  description = "The allocated storage for the DB instance"
+  description = "Allocated storage for the DB instance"
   type        = number
 }
 
@@ -305,63 +318,53 @@ variable "db_storage_encrypted" {
   type        = bool
 }
 
-variable "db_parameter_group_name" {
-  description = "The parameter group name for the DB instance"
-  type        = string
-}
-
-variable "db_option_group_name" {
-  description = "The option group name for the DB instance"
-  type        = string
-}
-
 variable "db_name" {
-  description = "The name of the database"
+  description = "Name of the database"
   type        = string
 }
 
 variable "db_username" {
-  description = "The username for the DB instance"
+  description = "Username for the database"
   type        = string
 }
 
 variable "db_password" {
-  description = "The password for the DB instance"
+  description = "Password for the database"
   type        = string
 }
 
 variable "db_port" {
-  description = "The port for the DB instance"
+  description = "Port for the DB instance"
   type        = number
 }
 
 variable "db_copy_tags_to_snapshot" {
-  description = "Copy tags to snapshot"
+  description = "Copy tags to snapshot for the DB instance"
   type        = bool
 }
 
 variable "db_monitoring_interval" {
-  description = "The monitoring interval for the DB instance"
+  description = "Monitoring interval for the DB instance"
   type        = number
 }
 
 variable "db_publicly_accessible" {
-  description = "Whether the DB instance is publicly accessible"
+  description = "Make the DB instance publicly accessible"
   type        = bool
 }
 
 variable "db_storage_type" {
-  description = "The storage type for the DB instance"
+  description = "Storage type for the DB instance"
   type        = string
 }
 
 variable "db_domain" {
-  description = "The domain for the DB instance"
+  description = "Domain for the DB instance"
   type        = string
 }
 
 variable "db_domain_iam_role_name" {
-  description = "The IAM role name for the DB domain"
+  description = "IAM role name for the DB instance domain"
   type        = string
 }
 
@@ -376,42 +379,42 @@ variable "db_skip_final_snapshot" {
 }
 
 variable "db_vpc_security_group_id" {
-  description = "The VPC security group ID for the DB instance"
-  type        = list(string)
+  description = "VPC security group ID for the DB instance"
+  type        = string
 }
 
 variable "db_backup_retention_period" {
-  description = "The backup retention period for the DB instance"
+  description = "Backup retention period for the DB instance"
   type        = number
 }
 
 variable "db_license_model" {
-  description = "The license model for the DB instance"
+  description = "License model for the DB instance"
   type        = string
 }
 
 variable "db_apply_immediately" {
-  description = "Apply changes immediately"
+  description = "Apply changes immediately for the DB instance"
   type        = bool
 }
 
 variable "db_availability_zone" {
-  description = "The availability zone for the DB instance"
+  description = "Availability zone for the DB instance"
   type        = string
 }
 
 variable "db_backup_window" {
-  description = "The backup window for the DB instance"
+  description = "Backup window for the DB instance"
   type        = string
 }
 
 variable "db_ca_cert_identifier" {
-  description = "The CA certificate identifier for the DB instance"
+  description = "CA certificate identifier for the DB instance"
   type        = string
 }
 
 variable "db_delete_automated_backups" {
-  description = "Delete automated backups"
+  description = "Delete automated backups for the DB instance"
   type        = bool
 }
 
@@ -421,87 +424,47 @@ variable "db_deletion_protection" {
 }
 
 variable "db_final_snapshot_identifier" {
-  description = "The final snapshot identifier for the DB instance"
+  description = "Final snapshot identifier for the DB instance"
   type        = string
 }
 
 variable "db_iam_database_authentication_enabled" {
-  description = "Enable IAM database authentication"
+  description = "Enable IAM database authentication for the DB instance"
   type        = bool
 }
 
 variable "db_iops" {
-  description = "The IOPS for the DB instance"
+  description = "IOPS for the DB instance"
   type        = number
-}
-
-variable "db_kms_key_id" {
-  description = "The KMS key ID for the DB instance"
-  type        = string
-}
-
-variable "db_maintenance_window" {
-  description = "The maintenance window for the DB instance"
-  type        = string
-}
-
-variable "db_max_allocated_storage" {
-  description = "The maximum allocated storage for the DB instance"
-  type        = number
-}
-
-variable "db_monitoring_role_arn" {
-  description = "The monitoring role ARN for the DB instance"
-  type        = string
-}
-
-variable "db_replicate_source_db" {
-  description = "The source DB instance for replication"
-  type        = string
-}
-
-variable "db_snapshot_identifier" {
-  description = "The snapshot identifier for the DB instance"
-  type        = string
-}
-
-variable "db_timezone" {
-  description = "The timezone for the DB instance"
-  type        = string
-}
-
-variable "db_timeouts" {
-  description = "The timeouts for the DB instance"
-  type        = map(string)
 }
 
 variable "kms_description" {
-  description = "The description of the KMS key"
+  description = "Description for the KMS key"
   type        = string
 }
 
 variable "kms_key_usage" {
-  description = "The usage of the KMS key"
+  description = "Key usage for the KMS key"
   type        = string
 }
 
 variable "kms_customer_master_key_spec" {
-  description = "The customer master key spec for the KMS key"
+  description = "Customer master key spec for the KMS key"
   type        = string
 }
 
 variable "kms_policy" {
-  description = "The policy for the KMS key"
+  description = "Policy for the KMS key"
   type        = string
 }
 
 variable "kms_deletion_window_in_days" {
-  description = "The deletion window in days for the KMS key"
+  description = "Deletion window in days for the KMS key"
   type        = number
 }
 
 variable "kms_is_enabled" {
-  description = "Whether the KMS key is enabled"
+  description = "Enable the KMS key"
   type        = bool
 }
 
